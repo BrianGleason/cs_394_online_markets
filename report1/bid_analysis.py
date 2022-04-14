1adc39760986dc389f17a1d02d5e38ac07ab1fe4
Bradley Pawlow <bradleypawlow99@gmail.com>
2022-04-13 22:26:56 -0500

    percent calculations

--- report1/bid_analysis.py
+++ report1/bid_analysis.py
@@ -91,7 +91,26 @@ def visualize(bids_1, values_1, bids_2, values_2):
     plt.ylabel("Actual Bid, b")
     plt.title("Bidder Value vs. Actual Bids (Auction A and Auction B)")
     plt.legend(["Auction A" , "Auction B"])
+    
+    plt.savefig('val_vid_scatterplot.png')
+
     plt.show()
+
+
+def perc_calc(bids, values):
+
+    count = 0
+    total_perc = 0
+    for i in range(len(bids)):
+        if bids[i] == values[i] / 2:
+            count += 1
+        total_perc += (bids[i] / values[i]) 
+
+    equil_perc = (count / len(bids)) * 100
+
+    avg_perc = (total_perc / len(bids)) * 100
+
+    return round(equil_perc, 2), round(avg_perc, 2)
 
 
 def analyze_netid(netid_1, netid_2):
@@ -131,6 +150,24 @@ def analyze_netid(netid_1, netid_2):
     #preprocessing auctions for visualization 
     auction_bid_a, auction_val_a = preprocess_auction(auction_a)
     auction_bid_b, auction_val_b = preprocess_auction(auction_b)
+
+    #combine vids and values from both auctions
+
+    total_bids = auction_bid_a + auction_bid_b
+
+    total_values = auction_val_a + auction_val_b
+
+    #Calculate what percent found the Bayes-Nash equilibrium and average percent of total value 
+
+    equil_perc = perc_calc(total_bids, total_values)[0]
+
+    avg_perc = perc_calc(total_bids, total_values)[1]
+
+    print(f'Percent of bids that are Bayes-Nash Equilibrium: {equil_perc}%')
+
+    print(f'Average percent of total value: {avg_perc}%')
+
+    #visualize scatterplot
 
     visualize(auction_bid_a, auction_val_a, auction_bid_b, auction_val_b)
   
